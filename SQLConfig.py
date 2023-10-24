15fa52cd8257137d6fff870d66ee8bd5872dd600
Samshh <samdacs2@gmail.com>
2023-10-25 00:04:56 +0800

    clear

--- SQLConfig.py
+++ SQLConfig.py
@@ -1,15 +1,1 @@
 import pyodbc
-
-# Replace these placeholders with your actual database details
-server = 'your_server_name'
-database = 'your_database_name'
-username = 'your_username'
-password = 'your_password'
-
-# Create a connection
-conn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}')
-
-# Create a cursor
-cursor = conn.cursor()
-cursor.close()
-conn.close()
